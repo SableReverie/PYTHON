7349a30fa79fd4dc828e181f222cf728954f65ce
Prince <princecultura10@gmail.com>
2025-10-25 18:06:28 +0800

    Add variables and print statement in 1.py

---
+++ 1.py
@@ -0,0 +1,18 @@
+# VARIABLES = STRING, FLOAT, BOOLEAN
+
+#STRINGS
+First_name = "Bro"
+food = "pizza"
+email = "Bro@gmail.com"
+
+#INTEGERS
+age = 25
+quantity = 3
+num_of_students = 5;
+
+#FLOAT 
+price = 10.99
+
+print(f"The price is ${price}")
+
+
